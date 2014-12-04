161105b7fdfff95f41d6ae91c53fc4f4221fd0e7
Leou <abccbaandy@gmail.com>
2014-12-04 15:05:58 +0800

    url check unique

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,6 +4,11 @@ class ApiController < ApplicationController
 		@doc = Nokogiri::HTML(open("http://w2.goodtv.tv/studio_classroom/"))
 		url = @doc.at_css("video")[:src]
 		link = Link.new(:url=>url)
-		render :json => {:url => url, :status => link.save}
+		if link.save
+			render :json => {:url => url }
+		else
+			render :json => {:url => url, :errors => link.errors }
+		end
+		
 	end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ActiveRecord::Base
+	validates_uniqueness_of :url
 	def self.check
     	Link.destroy_all
   	end
